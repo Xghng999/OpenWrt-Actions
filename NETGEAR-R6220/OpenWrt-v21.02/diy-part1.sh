9336d49d3ec9c6a26065a4d543f89892fb833d88
Xghng999 <1369492@qq.com>
2026-07-14 14:29:13 +0800

    Update diy-part1.sh

--- NETGEAR-R6220/OpenWrt-v21.02/diy-part1.sh
+++ NETGEAR-R6220/OpenWrt-v21.02/diy-part1.sh
@@ -29,4 +29,4 @@ sed -i 's/^\(.*packages\)/#&/' feeds.conf.default
 
 # 添加修改的packages源
 # sed -i '$a src-git packages https://github.com/lxhao61/openwrt-packages.git;19.07' feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git packages https://github.com/immortalwrt/packages.git;openwrt-21.02' feeds.conf.default
