60626b26543d5f83c4638a4663e7c813bbb8af0e
Xghng999 <1369492@qq.com>
2026-07-14 11:29:26 +0800

    Update diy-part1.sh

--- NETGEAR-R6220/OpenWrt-v21.02/diy-part1.sh
+++ NETGEAR-R6220/OpenWrt-v21.02/diy-part1.sh
@@ -12,11 +12,15 @@
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/^#\(.*smpackage\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*smpackage\)/\1/' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
+# 即使你使用的是官方 OpenWrt 源码，也可以通过这种方式拉取 ImmortalWrt 的插件
+# echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git' >>feeds.conf.default
+
 # 3. (可选) 添加其他常用的依赖源，确保编译不报错
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+echo 'src-git small-package https://github.com/kenzok8/small-package.git' >>feeds.conf.default
