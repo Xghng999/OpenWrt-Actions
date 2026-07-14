fe744bbb86fc4cbb8711963ceae67b43b122b38a
Xghng999 <1369492@qq.com>
2026-07-14 10:00:33 +0800

    Update diy-part1.sh

--- NETGEAR-R6220/OpenWrt-v21.02/diy-part1.sh
+++ NETGEAR-R6220/OpenWrt-v21.02/diy-part1.sh
@@ -12,7 +12,11 @@
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*smpackage\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # 3. (可选) 添加其他常用的依赖源，确保编译不报错
-# echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
-# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
