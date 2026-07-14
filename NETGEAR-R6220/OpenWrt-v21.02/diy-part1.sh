d24ad39ee6df95a02491af4b2c013bc687ba8348
Xghng999 <1369492@qq.com>
2026-07-14 11:39:25 +0800

    Update diy-part1.sh

--- NETGEAR-R6220/OpenWrt-v21.02/diy-part1.sh
+++ NETGEAR-R6220/OpenWrt-v21.02/diy-part1.sh
@@ -22,5 +22,5 @@
 # echo 'src-git homeproxy https://github.com/immortalwrt/homeproxy.git' >>feeds.conf.default
 
 # 3. (可选) 添加其他常用的依赖源，确保编译不报错
-# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
-echo 'src-git small-package https://github.com/kenzok8/small-package.git' >>feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
