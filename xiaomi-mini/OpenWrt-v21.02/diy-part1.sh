80bf2be4285b45a2dc440f09a816965a23815ea1
Xghng999 <1369492@qq.com>
2026-07-17 11:40:58 +0800

    Update diy-part1.sh

--- xiaomi-mini/OpenWrt-v21.02/diy-part1.sh
+++ xiaomi-mini/OpenWrt-v21.02/diy-part1.sh
@@ -18,10 +18,10 @@
 # sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 # 添加修改的packages源
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
-./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
-git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
-./scripts/feeds install -a
-make menuconfig
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+# rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
+# git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
+# ./scripts/feeds install -a
+# make menuconfig
