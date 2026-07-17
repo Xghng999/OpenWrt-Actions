4b80f1eb4d2de0a6abc7ce0ecabe4fc0614d81b7
Xghng999 <1369492@qq.com>
2026-07-17 16:32:56 +0800

    Create diy-part1.sh

---
+++ PSG1218-Ax/OpenWrt-v21.02/diy-part1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# 3. (可选) 添加其他常用的依赖源，确保编译不报错
+# echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
+# 添加修改的packages源
+# sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+# ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+# rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
+# git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
+# ./scripts/feeds install -a
+# make menuconfig
