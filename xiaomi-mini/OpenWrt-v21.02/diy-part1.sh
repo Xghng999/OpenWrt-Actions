1b925e737032adc2de397b4f381d0ab42c2de3fb
Xghng999 <1369492@qq.com>
2026-07-24 13:37:01 +0800

    Update diy-part1.sh

--- xiaomi-mini/OpenWrt-v21.02/diy-part1.sh
+++ xiaomi-mini/OpenWrt-v21.02/diy-part1.sh
@@ -15,7 +15,7 @@
 
 # 3. (可选) 添加其他常用的依赖源，确保编译不报错
 # echo 'src-git packages https://github.com/immortalwrt/packages.git' >>feeds.conf.default
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 # 添加修改的packages源
 # sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
